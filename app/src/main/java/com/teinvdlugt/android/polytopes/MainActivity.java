5d6aa9c4c23ffb00e6943ce85b60630ee976bfd8
Tein van der Lugt <teinvdlugt@gmail.com>
2016-03-25 17:11:14 +0100

    Switch between dimensions during runtime

--- app/src/main/java/com/teinvdlugt/android/polytopes/MainActivity.java
+++ app/src/main/java/com/teinvdlugt/android/polytopes/MainActivity.java
@@ -1,13 +1,34 @@
 package com.teinvdlugt.android.polytopes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PolytopeView polytopeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        polytopeView = (PolytopeView) findViewById(R.id.polytopeView);
+    }
+
+    public void onClick2D(View view) {
+        polytopeView.setDimensions(2);
+    }
+
+    public void onClick3D(View view) {
+        polytopeView.setDimensions(3);
+    }
+
+    public void onClick4D(View view) {
+        polytopeView.setDimensions(4);
+    }
+
+    public void onClick5D(View view) {
+        polytopeView.setDimensions(5);
     }
 }
--- app/src/main/java/com/teinvdlugt/android/polytopes/PolytopeView.java
+++ app/src/main/java/com/teinvdlugt/android/polytopes/PolytopeView.java
@@ -17,26 +17,30 @@ public class PolytopeView extends View {
     private double distanceInLowDimensions = 200;
     private double distancePerHigherDimension = 100;
     private Paint paint;
+    List<Point> points = new ArrayList<>();
+    List<Line> lines = new ArrayList<>();
+
+    private void render() {
+        points.clear();
+        lines.clear();
+        Point first = new Point(200, 1150);
+        points.add(first);
+        for (int n = 1; n < dimensions + 1; n++) {
+            dimensionUp(n);
+        }
+    }
 
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        List<Point> points = new ArrayList<>();
-        List<Line> lines = new ArrayList<>();
-        Point first = new Point(200, 1150);
-        points.add(first);
-        for (int n = 1; n < dimensions + 1; n++) {
-            dimensionUp(points, lines, n);
-        }
-
         for (Line line : lines) {
             canvas.drawLine(line.p1.x, line.p1.y, line.p2.x, line.p2.y, paint);
         }
     }
 
-    private void dimensionUp(List<Point> points, List<Line> lines, int dimension) {
+    private void dimensionUp(int dimension) {
         List<Point> newPoints = new ArrayList<>();
         List<Line> newLines = new ArrayList<>();
         for (Point point : points) {
@@ -76,10 +80,24 @@ public class PolytopeView extends View {
         lines.addAll(newLines);
     }
 
+    public int getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(int dimensions) {
+        boolean render = this.dimensions != dimensions;
+        this.dimensions = dimensions;
+        if (render) {
+            render();
+            invalidate();
+        }
+    }
+
     private void init() {
         paint = new Paint();
         paint.setStrokeWidth(5);
         paint.setAntiAlias(true);
+        render();
     }
 
     public PolytopeView(Context context) {
